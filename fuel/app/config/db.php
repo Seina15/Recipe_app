3feeba9d1a09fbf6d2e9439316297e3f8be59165
Seina <inagawa.15.ai@gmail.com>
2025-08-25 14:33:53 +0900

    environment construction

---
+++ fuel/app/config/db.php
@@ -0,0 +1,12 @@
+<?php
+
+return array(
+'connection' => [
+  'hostname' => '127.0.0.1',
+  'port'     => 3307,
+  'database' => 'recipe_app',
+  'username' => 'root',
+  'password' => 'root',
+],
+
+);
