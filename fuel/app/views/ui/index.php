b54fbe79e7a8fa01a567d4f8375a8bdc2d39d19c
Seina <inagawa.15.ai@gmail.com>
2025-08-27 12:37:04 +0900

    add: button

--- fuel/app/views/ui/index.php
+++ fuel/app/views/ui/index.php
@@ -10,22 +10,45 @@
 
 <body>
     <?php echo View::forge('parts/header'); ?>
-    <section>
+
     <section class="main-sections">
 
+        <!-- 左画面（レコメンド） -->
         <div class="left-section">
+            <div class="section-header">
+                <h1>Recommend</h1>
+                
+                <button class="icon-btn" aria-label="filter-recommend" >
+                    <i class="fa-solid fa-filter"></i>
+                </button>
 
-            <!-- 左画面（レコメンド） -->
-            <h1 style="font-size: 30px; margin-left: 20px;">Recommend</h1>
+                <button class="icon-btn" aria-label="filter-recommend" >
+                    <i class="fa-solid fa-magnifying-glass"></i>
+                </button>
+                
+            </div>
+
+
             <hr class="section-div"/>
-            
+        </div>
+
+        <!-- 右画面（ショッピングリスト） -->
+        <div class="right-section">
+            <div class="section-header" style="display: flex; align-items: center; gap: 20px;">
+                <h1>Shopping List</h1>
+                
+                <button class="icon-btn" aria-label="clear-list" style="background-color: #da8660;">
+                    <i class="fa-solid fa-trash-can"></i>
+                </button>
+
+                <button class="icon-btn" aria-label="add-item" style="background-color: #4CAF50;">
+                    <i class="fa-solid fa-circle-plus"></i>
+                </button>
             </div>
 
-            <!-- 右画面（ショッピングリスト） -->
-            <div class="right-section">
-                <h1 style="font-size: 30px;">Shopping List</h1>
-                <hr class="section-div"/>
-                <?php echo View::forge('parts/right_section', ['items' => $items]); ?>
-
-            </div>
+            <hr class="section-div">
+            <?php echo View::forge('parts/right_section', ['items' => $items]); ?>
+        </div>
     </section>
+</body>
+</html>
