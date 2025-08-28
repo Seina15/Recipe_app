5c1e113ec6703800b7c4d9cb947935a229326ccc
Seina <inagawa.15.ai@gmail.com>
2025-08-28 17:57:00 +0900

    modify: compose.yaml

---
+++ fuel/app/config/production/rakuten_recipe.php
@@ -0,0 +1,6 @@
+<?php
+return [
+  'application_id' => getenv('RAKUTEN_APP_ID'),
+  'base' => 'https://app.rakuten.co.jp/services/api/Recipe',
+  'timeout' => 5,
+];
---
+++ fuel/app/logs/2025/08/28.php
@@ -0,0 +1,60 @@
+<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
+
+WARNING - 2025-08-28 16:44:46 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2025-08-28 08:18:16 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2025-08-28 08:18:18 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+ERROR - 2025-08-28 08:18:18 --> Runtime Deprecated code usage - Use of "static" in callables is deprecated in /home/c0b230162c/Recipe/fuel/core/classes/security.php on line 148
+WARNING - 2025-08-28 08:19:05 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+ERROR - 2025-08-28 08:19:05 --> Compile Error - Cannot declare class Controller_Ping, because the name is already in use in /home/c0b230162c/Recipe/fuel/app/classes/controller/png.php on line 2
+WARNING - 2025-08-28 08:20:03 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+ERROR - 2025-08-28 08:20:03 --> Compile Error - Cannot declare class Controller_PingTest, because the name is already in use in /home/c0b230162c/Recipe/fuel/app/classes/controller/png.php on line 2
+WARNING - 2025-08-28 08:20:03 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+ERROR - 2025-08-28 08:20:03 --> Compile Error - Cannot declare class Controller_PingTest, because the name is already in use in /home/c0b230162c/Recipe/fuel/app/classes/controller/png.php on line 2
+WARNING - 2025-08-28 08:20:05 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+ERROR - 2025-08-28 08:20:05 --> Compile Error - Cannot declare class Controller_PingTest, because the name is already in use in /home/c0b230162c/Recipe/fuel/app/classes/controller/png.php on line 2
+WARNING - 2025-08-28 08:21:10 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2025-08-28 17:23:55 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2025-08-28 17:34:41 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+ERROR - 2025-08-28 17:34:41 --> Error - syntax error, unexpected 'class' (T_CLASS), expecting function (T_FUNCTION) or const (T_CONST) in /var/www/html/my_fuel_project/fuel/app/classes/controller/home.php on line 23
+WARNING - 2025-08-28 17:34:41 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+ERROR - 2025-08-28 17:34:41 --> Error - syntax error, unexpected 'class' (T_CLASS), expecting function (T_FUNCTION) or const (T_CONST) in /var/www/html/my_fuel_project/fuel/app/classes/controller/home.php on line 23
+WARNING - 2025-08-28 17:36:00 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+ERROR - 2025-08-28 17:36:00 --> Error - syntax error, unexpected 'class' (T_CLASS), expecting function (T_FUNCTION) or const (T_CONST) in /var/www/html/my_fuel_project/fuel/app/classes/controller/home.php on line 23
+WARNING - 2025-08-28 17:36:00 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+ERROR - 2025-08-28 17:36:00 --> Error - syntax error, unexpected 'class' (T_CLASS), expecting function (T_FUNCTION) or const (T_CONST) in /var/www/html/my_fuel_project/fuel/app/classes/controller/home.php on line 23
+WARNING - 2025-08-28 17:36:38 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+ERROR - 2025-08-28 17:36:38 --> Error - Could not find asset: bootstrap.css in /var/www/html/my_fuel_project/fuel/core/classes/asset/instance.php on line 413
+WARNING - 2025-08-28 17:36:38 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+ERROR - 2025-08-28 17:36:38 --> Error - Could not find asset: bootstrap.css in /var/www/html/my_fuel_project/fuel/core/classes/asset/instance.php on line 413
+WARNING - 2025-08-28 17:36:39 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+ERROR - 2025-08-28 17:36:39 --> Error - Could not find asset: bootstrap.css in /var/www/html/my_fuel_project/fuel/core/classes/asset/instance.php on line 413
+WARNING - 2025-08-28 17:36:39 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+ERROR - 2025-08-28 17:36:39 --> Error - Could not find asset: bootstrap.css in /var/www/html/my_fuel_project/fuel/core/classes/asset/instance.php on line 413
+WARNING - 2025-08-28 17:37:47 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2025-08-28 17:37:47 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2025-08-28 17:44:51 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+ERROR - 2025-08-28 17:44:51 --> Error - Could not find asset: bootstrap.css in /var/www/html/my_fuel_project/fuel/core/classes/asset/instance.php on line 413
+WARNING - 2025-08-28 17:44:53 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+ERROR - 2025-08-28 17:44:53 --> Error - Could not find asset: bootstrap.css in /var/www/html/my_fuel_project/fuel/core/classes/asset/instance.php on line 413
+WARNING - 2025-08-28 17:44:53 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+ERROR - 2025-08-28 17:44:53 --> Error - Could not find asset: bootstrap.css in /var/www/html/my_fuel_project/fuel/core/classes/asset/instance.php on line 413
+WARNING - 2025-08-28 17:46:24 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+ERROR - 2025-08-28 17:46:24 --> Error - Could not find asset: bootstrap.css in /var/www/html/my_fuel_project/fuel/core/classes/asset/instance.php on line 413
+WARNING - 2025-08-28 17:46:24 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+ERROR - 2025-08-28 17:46:24 --> Error - Could not find asset: bootstrap.css in /var/www/html/my_fuel_project/fuel/core/classes/asset/instance.php on line 413
+WARNING - 2025-08-28 17:46:24 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+ERROR - 2025-08-28 17:46:24 --> Error - Could not find asset: bootstrap.css in /var/www/html/my_fuel_project/fuel/core/classes/asset/instance.php on line 413
+WARNING - 2025-08-28 17:46:24 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+ERROR - 2025-08-28 17:46:24 --> Error - Could not find asset: bootstrap.css in /var/www/html/my_fuel_project/fuel/core/classes/asset/instance.php on line 413
+WARNING - 2025-08-28 17:46:25 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+ERROR - 2025-08-28 17:46:25 --> Error - Could not find asset: bootstrap.css in /var/www/html/my_fuel_project/fuel/core/classes/asset/instance.php on line 413
+WARNING - 2025-08-28 17:46:25 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+ERROR - 2025-08-28 17:46:25 --> Error - Could not find asset: bootstrap.css in /var/www/html/my_fuel_project/fuel/core/classes/asset/instance.php on line 413
+WARNING - 2025-08-28 17:49:44 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2025-08-28 17:49:44 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2025-08-28 17:49:44 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2025-08-28 17:50:04 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2025-08-28 17:50:04 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2025-08-28 17:50:04 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2025-08-28 17:55:38 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+ERROR - 2025-08-28 17:55:38 --> Error - Could not find asset: bootstrap.css in /var/www/html/my_fuel_project/fuel/core/classes/asset/instance.php on line 413
