d4fe04335a76cd4c97afe5ce264226428abb1b2d
Seina <inagawa.15.ai@gmail.com>
2025-09-14 13:11:54 +0900

    update: README

---
+++ queries/user_profile.sql
@@ -0,0 +1,13 @@
+CREATE TABLE `user_profile` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `user_id` INT NOT NULL,
+  `avoid` TEXT NULL,
+  `time` INT NULL,
+  `budget` INT NULL,
+  `servings` INT NULL,
+  `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uk_user` (`user_id`)
+)
+DEFAULT CHARSET=utf8mb4
+COLLATE=utf8mb4_unicode_ci;
